9f702d2348103a43fdde98f445e0e86c9cfef05d
github jirou6699 <shundom09@gmail.com>
2022-11-01 23:39:09 +0900

    initial commit

---
+++ lib/sample.rb
@@ -0,0 +1,3 @@
+def section(a, b)
+	puts a + b
+end
---
+++ lib/sample2.rb
@@ -0,0 +1,16 @@
+require_relative 'sample'
+
+# puts section(1, 4)
+
+users = [
+	{name: "太郎" , age: 30},
+	{name: "良しろる", age: 20},
+	{name: "シンジくん", age: 20},
+	{name: "シンジくん", age: 20},
+	{name: "シンジくん", age: 20},
+	{name: "シンジくん", age: 20},
+	{name: "シンジくん", age: 20}
+]
+
+p users
+pp users
